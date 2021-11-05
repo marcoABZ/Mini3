5bdb956733ca508fb60ee90a9df7fb910f669486
Marco Zulian <marco.ab.zulian@gmail.com>
2021-11-05 14:42:36 -0300

    Initial Commit

---
+++ Mini3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mini3
+//
+//  Created by Marco Zulian on 05/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mini3/Mini3App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini3App.swift
+//  Mini3
+//
+//  Created by Marco Zulian on 05/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Mini3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
