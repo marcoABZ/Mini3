be24dcb9c1c56c21c8b099d56eb168a93e95b514
Marco Zulian <marco.ab.zulian@gmail.com>
2021-11-16 13:57:52 -0300

    Commit de arquivos que não tinham sido submetidos

---
+++ Mini3/Quebra-Cabeca/Views/PuzzlePieceView.swift
@@ -0,0 +1,59 @@
+//
+//  PuzzlePieceView.swift
+//  Mini3
+//
+//  Created by Marco Zulian on 16/11/21.
+//
+
+import SwiftUI
+
+struct PuzzlePieceView: View {
+    @EnvironmentObject var student: Profile
+    @ObservedObject var puzzleManager: PuzzleManager
+    @ObservedObject var piece: PuzzlePieceManager<UIImage>
+    var preview: Bool = false
+    
+    var body: some View {
+            ZStack {
+                if puzzleManager.settings.ordenacao {
+                    Image(uiImage: piece.content)
+                        .overlay (alignment: .bottom) {
+                            Rectangle()
+                                .foregroundColor(student.color)
+                                .frame(height: 50)
+                            Text(String(piece.index + 1))
+                                .foregroundColor(.white)
+                                .font(.system(size: 36, weight: .bold, design: .default))
+                        }
+                } else {
+                    Image(uiImage: piece.content)
+                }
+            }
+            .offset(piece.displacement)
+            .zIndex(piece.displacement == .zero ? 0 : 1)
+            .gesture(
+                DragGesture(coordinateSpace: .global)
+                    .onChanged {
+                        if !piece.isCorrect {
+                            piece.drag(forDistance: CGSize(width: $0.translation.width, height: $0.translation.height))                      }
+                    }
+                    .onEnded { _ in
+                        piece.drop()
+                        puzzleManager.updateGameStatus()
+                        print(puzzleManager.isOver)
+                    }
+            )
+            .shadow(color: student.color, radius: piece.isCorrect ? 10 : 0)
+            .overlay(
+                GeometryReader { geo in
+                    Color.clear
+                        .onAppear {
+                            if !preview {
+                                piece.setStartPosition(at: geo.frame(in: .global))
+                            }
+                        }
+                }
+            )
+    }
+}
+
