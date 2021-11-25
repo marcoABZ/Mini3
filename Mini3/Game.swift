2554d3cc4a368957b8ea75e64ebf93c366b5bb61
Marco Zulian <marco.ab.zulian@gmail.com>
2021-11-25 13:43:19 -0300

    Ajustes no enum Game (reposicionamento do método getCover e criação do método getDescricao). GameDashboardView utiliza essas mudanças para criar os jogos a partir dos casos em Game. DashboardManager deixa de ser responsável por esse controle.

--- Mini3/Game.swift
+++ Mini3/Game.swift
@@ -23,4 +23,39 @@ enum Game: String, CaseIterable {
         
         return false
     }
+    
+    func getCoverImage(mascote: Mascotes) -> Image {
+        var aux = ""
+        switch self {
+        case .quebraCabeca:
+            aux = "1"
+        case .formas:
+            aux = "2"
+        case .sonsEimagens:
+            aux = "3"
+        case .palheta:
+            aux = "4"
+        case .imagensEformas:
+            aux = "5"
+        }
+        
+        switch mascote {
+            case .coelho:
+                return Image("coelhoMini\(aux)Cover")
+            case .chiba:
+                return Image("chibaMini\(aux)Cover")
+            case .gato:
+                return Image("gatoMini\(aux)Cover")
+        }
+    }
+    
+    func getDescription() -> String {
+        switch self {
+        case .quebraCabeca:
+            return "Organize e monte a imagem."
+        default:
+            return "Jogo em desenvolvimento"
+        }
+    }
+    
 }
